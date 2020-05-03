b263927c139f4c1d79dde78e3bfb3fadfe7faba7
Andrew Skurlatov <a.skurlatov@gmail.com>
2020-05-03 21:00:42 +0300

    fix: init scripts

--- scripts/activate_validators.sh
+++ scripts/activate_validators.sh
@@ -3,13 +3,13 @@
 # scripts fro autmaticaly insert all keys to validators
 
 set -a
-. ./keys/thrall/thrall.env
+. ./keys/thrall/.env
 set +a
 
 ./scripts/import_keys.sh
 
 set -a
-. ./keys/jaina/jaina.env
+. ./keys/jaina/.env
 set +a
 
 ./scripts/import_keys.sh http://localhost:9934
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+# up all composer services
 docker-compose up -d
+
+# wait all nodes running
+sleep 3
+
+# activate validator nodes
 ./scripts/activate_validators.sh
+
+# restart validator nodes for start finalizing blocks
+# (don't know why they don't do it automatically after key imports)
+docker-compose restart thrall jaina
